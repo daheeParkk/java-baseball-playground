d9bc8902a3faf5de14239c26e56d948eb0e74bb0
daheeParkk <qkrekgml7414@naver.com>
2023-03-16 02:47:39 +0900

    refactor : randomNums 타입 변경

--- src/main/java/game/NumbersBaseballGame.java
+++ src/main/java/game/NumbersBaseballGame.java
@@ -1,24 +1,71 @@
 package game;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.Stream;
 
 public class NumbersBaseballGame {
 
-    public int[] generateRandomNumbers() {
+    public ArrayList<Integer> generateRandomNumbers() {
 
         Random random = new Random();
-        int[] randomNums = new int[3];
+        ArrayList<Integer> randomNums = new ArrayList<>();
         ArrayList<Integer> numberList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
 
         for (int i=0; i<3; i++) {
 
             int index = random.nextInt(numberList.size()-1);
-            randomNums[i] = numberList.get(index);
+            randomNums.add(i, numberList.get(index));
             numberList.remove(index);
         }
 
         return randomNums;
+    }
+
+    public int[] compareNumbers(String inputNumber, ArrayList<Integer> randomNums) {
+
+        int strike = 0;
+        int ball = 0;
+
+        int[] myNums = stringToIntArray(inputNumber);
+
+        for (int i=0; i<3; i++) {
+
+            strike += checkStrike(myNums, randomNums, i);
+            ball += checkBall(myNums, randomNums, i);
+        }
+
+        return new int[]{strike, ball};
+    }
+
+    private int[] stringToIntArray(String inputNumber) {
+
+        int[] myNums;
+
+        myNums = Stream.of(inputNumber.split("")).mapToInt(Integer::parseInt).toArray();
+
+        return myNums;
+    }
+
+    private int checkStrike(int[] myNums, ArrayList<Integer> randomNums, int order) {
+
+        if (myNums[order] == randomNums.get(order)) {
+
+            return 1;
+        }
+
+        return 0;
+    }
+
+    private int checkBall(int[] myNums, ArrayList<Integer> randomNums, int order) {
+
+        if (myNums[order] != randomNums.get(order) && randomNums.contains(myNums[order])) {
+
+            return 1;
+        }
+
+        return 0;
     }
 }
--- src/test/java/baseball/NumbersBaseballGameTest.java
+++ src/test/java/baseball/NumbersBaseballGameTest.java
@@ -3,6 +3,8 @@ package baseball;
 import game.NumbersBaseballGame;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class NumbersBaseballGameTest {
@@ -11,8 +13,20 @@ public class NumbersBaseballGameTest {
     public void generateRandomNumbersTest() {
 
         NumbersBaseballGame numbersBaseballGame = new NumbersBaseballGame();
-        int[] randomNumbers = numbersBaseballGame.generateRandomNumbers();
+        ArrayList<Integer> randomNumbers = numbersBaseballGame.generateRandomNumbers();
         assertThat(randomNumbers).doesNotContain(0);
-        assertThat(randomNumbers.length).isEqualTo(3);
+        assertThat(randomNumbers.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void compareNumbersTest() {
+
+        NumbersBaseballGame numbersBaseballGame = new NumbersBaseballGame();
+        ArrayList<Integer> randomNumbers = new ArrayList<>();
+        randomNumbers.add(1);
+        randomNumbers.add(2);
+        randomNumbers.add(3);
+        int[] strikeAndBall = numbersBaseballGame.compareNumbers("253", randomNumbers);
+        assertThat(strikeAndBall).containsOnly(1,1);
     }
 }
