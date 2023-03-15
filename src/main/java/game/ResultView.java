bf68f1b5331f6f87db54d64e73f6a5a75229143e
daheeParkk <qkrekgml7414@naver.com>
2023-03-16 03:24:50 +0900

    feat : 종료 or다시 시작 선택하는 기능 구현

--- src/main/java/game/ResultView.java
+++ src/main/java/game/ResultView.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.util.Scanner;
+
 public class ResultView {
 
     public String  returnResult(int[] strikeAndBall) {
@@ -14,11 +16,28 @@ public class ResultView {
             return "\n"+strikeAndBall[0]+"스트라이크\n";
         }
 
+        if (strikeAndBall[0] == 3) {
+
+            return collectAnswer(strikeAndBall);
+        }
+
         if (strikeAndBall[0] == 0 && strikeAndBall[1] > 0) {
 
             return "\n"+strikeAndBall[1]+"볼\n";
         }
 
         return "\n낫싱\n";
+    }
+
+    private String collectAnswer(int[] strikeAndBall) {
+
+        System.out.println("\n3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.nextLine().equals("1")) {
+            return "계속";
+        }
+        return "종료";
+
     }
 }
