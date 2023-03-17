1783b462ed45978938d5fef8699e185f8fe301fc
daheeParkk <qkrekgml7414@naver.com>
2023-03-18 01:16:15 +0900

    refactor : 3개만 입력 가능하도록 수정

--- src/main/java/baseballGame/InputView.java
+++ src/main/java/baseballGame/InputView.java
@@ -8,7 +8,14 @@ public class InputView {
     public String inputBalls() {
 
         System.out.print("숫자를 입력해 주세요 : ");
-        return scanner.nextLine();
+        String inputBalls = scanner.nextLine();
+        while (inputBalls.length() != 3) {
+
+            System.out.println("숫자 3개를 다시 입력해 주세요");
+            inputBalls = scanner.nextLine();
+        }
+
+        return inputBalls;
     }
 
     public void restart() {
