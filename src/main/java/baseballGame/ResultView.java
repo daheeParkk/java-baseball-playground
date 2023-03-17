39673ee319cf6d6f80b4b074afec50a856743a02
daheeParkk <qkrekgml7414@naver.com>
2023-03-17 23:44:45 +0900

    feat : 힌트를 출력하는 기능 구현

---
+++ src/main/java/baseballGame/ResultView.java
@@ -0,0 +1,46 @@
+package baseballGame;
+
+public class ResultView {
+
+    public Boolean hintOutput(int[] result) {
+
+        int numOfBall = result[0];
+        int numOfStrike = result[1];
+
+        Boolean isThreeStrike = separationOfAnswer(numOfBall, numOfStrike);
+
+        if (isThreeStrike) {
+
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            return true;
+        }
+        return false;
+    }
+
+    private Boolean separationOfAnswer(int ball, int strike) {
+
+        if (ball == 0 && strike == 0) {
+
+            System.out.print("낫싱\n");
+            return false;
+        }
+        if (ball != 0 && strike == 0) {
+
+            System.out.print(ball + "볼\n");
+            return false;
+        }
+        if (ball == 0) {
+
+            System.out.print(strike + "스트라이크\n");
+            return checkAnswer(strike);
+        }
+        System.out.print(ball + "볼 " + strike + "스트라이크\n");
+        return false;
+    }
+
+    private Boolean checkAnswer(int strike) {
+
+        return strike == 3;
+    }
+}
